9e5580fc7eee79594f0a8bf184e4ced74d94be6c
Irina Baeva <irina.baeva@bonial.com>
2021-01-24 12:31:03 +0100

    Fix bubble sort

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -6,19 +6,37 @@ def bubbleSort(list_a):
     while not sorted:
         sorted = True
         for i in range(0, indexing_length):
+            # print(list_a)
             if list_a[i] > list_a[i+1]: #check 2 numbers
                 sorted = False # when we have sorted items, this flag will not be activated and it will break the loop 
                 list_a[i], list_a[i+1] = list_a[i+1], list_a[i] #flip
-                # print(list_a)
     return list_a
         
 '''
 steps:
+[2, 6, 5, 4, 3, 7, 8]
+[2, 6, 5, 4, 3, 7, 8]
 [2, 5, 6, 4, 3, 7, 8]
 [2, 5, 4, 6, 3, 7, 8]
 [2, 5, 4, 3, 6, 7, 8]
+[2, 5, 4, 3, 6, 7, 8]
+[2, 5, 4, 3, 6, 7, 8]
+[2, 5, 4, 3, 6, 7, 8]
 [2, 4, 5, 3, 6, 7, 8]
 [2, 4, 3, 5, 6, 7, 8]
+[2, 4, 3, 5, 6, 7, 8]
+[2, 4, 3, 5, 6, 7, 8]
+[2, 4, 3, 5, 6, 7, 8]
+[2, 4, 3, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
+[2, 3, 4, 5, 6, 7, 8]
 [2, 3, 4, 5, 6, 7, 8]
 [2, 3, 4, 5, 6, 7, 8]
 '''
